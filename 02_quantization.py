5fa58e62074104fe050c9a8ea7e58661fca3d84d
Jiean Yang <jiean.yang.freelancer@gmail.com>
2025-04-11 17:50:47 +0200

    Update codes

--- 02_quantization.py
+++ 02_quantization.py
@@ -6,9 +6,25 @@ device = torch.device("mps" if torch.backends.mps.is_available() else "cpu")
 print("device:", device)
 
 
-def load_model():
-    model_name = "meta-llama/Meta-Llama-3.1-8B-Instruct"
-    model = AutoModelForCausalLM.from_pretrained(model_name, device_map="auto")
+def load_model_with_quantization():
+    model_name = "meta-llama/Llama-3.2-1B-Instruct"
+    # model_name = "meta-llama/Llama-3.2-3B-Instruct"
+
+    # 4-bit 量化配置（大幅减少内存占用）
+    # BitsAndBytesConfig not support MacOS M1
+    # bnb_config = BitsAndBytesConfig(
+    #     load_in_4bit=True,
+    #     bnb_4bit_quant_type="nf4",
+    #     bnb_4bit_use_double_quant=True,
+    #     bnb_4bit_compute_dtype=torch.bfloat16,  # 兼容 Apple Neural Engine
+    # )
+
+    # 加载模型（自动分配到 mps/cpu）
+    model = AutoModelForCausalLM.from_pretrained(
+        model_name,
+        # quantization_config=bnb_config,
+        device_map="auto",  # 自动选择 mps/cpu
+    )
     return model_name, model
 
 
@@ -29,7 +45,7 @@ def inference(model_name, model):
 
 
 if __name__ == "__main__":
-    model_name, model = load_model()
+    model_name, model = load_model_with_quantization()
     print("model_name", model_name)
     print("model", model)
     show_data_type_of_model_parameters_and_memory_footprints(model)
