0f2e222e3721fe1bbcb45e49aa7728af0aff5f12
Jiean Yang <jiean.yang.freelancer@gmail.com>
2025-04-11 16:14:26 +0200

    Rename from quantization.py to unquantization.py

--- unquantization.py
+++ unquantization.py
@@ -5,7 +5,7 @@ import torch
 def load_model():
     model_name = "meta-llama/Meta-Llama-3.1-8B-Instruct"
     model = AutoModelForCausalLM.from_pretrained(model_name, device_map="auto")
-    return model
+    return model_name, model
 
 
 def show_data_type_of_model_parameters_and_memory_footprints(model):
@@ -14,15 +14,17 @@ def show_data_type_of_model_parameters_and_memory_footprints(model):
     print("Memory footprints:", model.get_memory_footprint())
 
 
-def inference(model):
+def inference(model_name, model):
     tokenizer = AutoTokenizer.from_pretrained(model_name)
     input = tokenizer("Portugal is", return_tensors="pt").to("cuda")
+    print("input", input)
 
     response = model.generate(**input, max_new_tokens=50)
+    print("response", response)
     print(tokenizer.batch_decode(response, skip_special_tokens=True))
 
 
 if __name__ == "__main__":
-    model = load_model()
-    show_data_type_of_model_parameters_and_memory_footprints(model)
+    model_name, model = load_model()
+    show_data_type_of_model_parameters_and_memory_footprints(model_name, model)
     # inference(model)
